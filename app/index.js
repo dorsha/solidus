b573e96a5a931f3a2fae2d8e200daba41dba5a33
dorsha <doron@dorsha.com>
2014-10-16 22:25:03 +0300

    Fixed build #2 (morgan)

--- app/index.js
+++
@@ -1,7 +0,0 @@
-var compression = require('compression')
-var express = require('express');
-var app = express();
-
-app.use(express.logger('dev'));
-app.use(compression())
-app.listen(process.env.PORT || 5000);
