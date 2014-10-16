6bb5d039154694bb8976b9e3740cfc746ef57ca0
dorsha <doron@dorsha.com>
2014-10-16 22:31:45 +0300

    Fixed build #3

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+var express = require('express'),
+	morgan = require('morgan'),
+	bodyParser = require('gzippo');
+
+var app = express();
+
 var gzippo = require('gzippo');
 var morgan = require('morgan');
 var express = require('express');
 var app = express();
  
+app.set('port', process.env.PORT || 3000);
+ 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+
+app.listen(app.get('port'));
