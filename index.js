8d266d5f6af337be88f0163d124fa51fd2eb480b
dorsha <doron@dorsha.com>
2014-10-16 23:51:28 +0300

    Fixed build #20

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 //var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname));
+app.use(express.static("dist"));
 console.log("!!!!!!!!!!! " + __dirname + " !!!!!!!!!!!!!!!!!")
 app.listen(app.get('port'), function() {
   console.log("Express server listening on port: " + app.get('port'))
