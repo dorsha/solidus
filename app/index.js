a95ffe5490a5762cde5dd698953d653dd6e2b225
dorsha <doron@dorsha.com>
2014-10-16 22:09:37 +0300

    Fixed build

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-var gzippo = require('gzippo');
+var compression = require('compression')
 var express = require('express');
 var app = express();
- 
+
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(compression())
 app.listen(process.env.PORT || 5000);
