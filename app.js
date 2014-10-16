8f840e89f399924a5c375a4082eb1ce19f7e0613
dorsha <doron@dorsha.com>
2014-10-16 23:15:34 +0300

    Fixed build #16

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express'),
     morgan = require('morgan'),
     http = require('http'),
-    path = require('path'),
-    gzippo = require('gzippo');
+    path = require('path');
+    //gzippo = require('gzippo');
 
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
