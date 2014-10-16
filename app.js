74edae41ee72e32e038b0ace98be8f4cab6c2159
dorsha <doron@dorsha.com>
2014-10-16 23:07:08 +0300

    Fixed build #12

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     errorHandler = require('errorhandler'),
-    favicon = require('serve-favicon'),
     http = require('http'),
     path = require('path'),
     bodyParser = require('gzippo');
@@ -18,7 +17,6 @@ if ('development' == env){
 app.set('port', process.env.PORT || 5000);
 app.set('views', 'dist/views');
 //app.set('view engine', 'jade');
-app.use(favicon("dist/favicon.ico"));
 app.use(morgan('dev'));
 app.use(bodyParser());
 app.use(methodOverride());
