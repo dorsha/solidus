6f213b4c59278c2d640a087a4a1187bd05aba3c7
dorsha <doron@dorsha.com>
2014-10-17 02:52:39 +0300

    no message

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function() {
