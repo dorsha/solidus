cf7369a27938c92cf71832ffc82f947ae5a9dc41
dorsha <doron@dorsha.com>
2014-10-28 00:57:10 +0200

    Implemented the balance page

--- app/scripts/controllers/balance.js
+++ app/scripts/controllers/balance.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('solidusApp')
-    .controller('BalanceCtrl', function ($scope) {
-        $scope.values = [-5, 5];
+    .controller('BalanceCtrl', function ($scope, $rootScope) {
+
+        $scope.periodOptions = [
+            { id: 'month', title: $scope.appmessages.balanceDateMonthly },
+            { id: 'quarter', title: $scope.appmessages.balanceQuarter },
+            { id: 'year', title: $scope.appmessages.balanceYear },
+            { id: 'twoYears', title: $scope.appmessages.balanceTwoYears }
+        ];
+        $scope.periodSelected = function(period) {
+            $rootScope.selectedPeriodId = period.id;
+        };
+
+        if (!$rootScope.periodSliderValues) {
+            $rootScope.periodSliderValues = [-5, 5];
+        }
     });
